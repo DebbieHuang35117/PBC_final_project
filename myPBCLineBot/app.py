73cb4f1c45c198b8b76bb772d61da3fdfd6e3cf6
DebbieHuang35117 <88933653+DebbieHuang35117@users.noreply.github.com>
2022-01-02 15:52:58 +0800

    Update app.py

--- myPBCLineBot/app.py
+++ myPBCLineBot/app.py
@@ -1,3 +1,4 @@
+'''
 from flask import Flask, request, abort
 
 from linebot import (
@@ -103,3 +104,4 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+'''
